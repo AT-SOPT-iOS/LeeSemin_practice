4537184aa8195fbef90789af367b4c147c97b231
sem-git <lsmsm0327@naver.com>
2025-04-11 17:08:41 +0900

    chore: Pretendard 폰트 추가 및 info.plist 설정

---
+++ AT-SOPT-iOS-Practice/AT-SOPT-iOS-Practice/Extensions/UIFont+.swift
@@ -0,0 +1,38 @@
+//
+//  UIFont+.swift
+//  AT-SOPT-iOS-Practice
+//
+//  Created by 이세민 on 4/11/25.
+//
+
+import UIKit
+
+extension UIFont {
+    class var head1: UIFont {
+        return UIFont(name: "Pretendard-ExtraBold", size: 25)!
+    }
+    
+    class var subhead1: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 18)!
+    }
+    
+    class var subhead2: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 16)!
+    }
+    
+    class var subhead3: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 15)!
+    }
+    
+    class var subhead4: UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: 14)!
+    }
+    
+    class var subhead5: UIFont {
+        return UIFont(name: "Pretendard-Regular", size: 14)!
+    }
+    
+    class var info12: UIFont {
+        return UIFont(name: "Pretandard-Regular", size: 12)!
+    }
+}

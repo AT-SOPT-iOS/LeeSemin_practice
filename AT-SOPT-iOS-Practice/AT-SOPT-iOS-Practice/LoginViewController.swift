729fa4904cc2f71ea5cd233eca8d21bd47b239b2
sem-git <lsmsm0327@naver.com>
2025-04-11 15:11:02 +0900

    feat: LoginViewController UI 구현

---
+++ AT-SOPT-iOS-Practice/AT-SOPT-iOS-Practice/LoginViewController.swift
@@ -0,0 +1,76 @@
+//
+//  LoginViewController.swift
+//  AT-SOPT-iOS-Practice
+//
+//  Created by 이세민 on 4/5/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel(frame: centeredFrame(y: 163, width: 236, height: 42))
+        label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let idTextField: UITextField = {
+        let textField = UITextField(frame: centeredFrame(y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.layer.cornerRadius = 3
+        textField.clipsToBounds = true
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField(frame: centeredFrame(y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = .systemFont(ofSize: 14)
+        textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.layer.cornerRadius = 3
+        textField.clipsToBounds = true
+        return textField
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(frame: LoginViewController.centeredFrame(y: 422, width: 335, height: 57))
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.layer.cornerRadius = 6
+        button.clipsToBounds = true
+        //        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setUI()
+    }
+    
+    private func setStyle() {
+        self.view.backgroundColor = .white
+    }
+    
+    private func setUI() {
+        [titleLabel, idTextField, passwordTextField, loginButton].forEach {
+            self.view.addSubview($0)
+        }
+    }
+    
+    private static func centeredFrame(y: CGFloat, width: CGFloat, height: CGFloat) -> CGRect {
+        let screenWidth = UIScreen.main.bounds.width
+        let x = (screenWidth - width) / 2
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+}

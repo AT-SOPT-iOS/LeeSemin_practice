04eca8b4da30da2401c1224c9cd1d7acac3b42da
sem-git <lsmsm0327@naver.com>
2025-05-06 14:01:31 +0900

    network: 내 닉네임 조회 서비스 로직 추가

--- AT-SOPT-iOS-Practice/AT-SOPT-iOS-Practice/Week04/Network/Nickname/GetInfoService.swift
+++ AT-SOPT-iOS-Practice/AT-SOPT-iOS-Practice/Week04/Network/Nickname/GetInfoService.swift
@@ -12,7 +12,7 @@ final class GetInfoService {
     static let shared = GetInfoService()
     private init() {}
     
-    func makeRequest(keyword: String?) -> URLRequest? {
+    private func makeNicknameListRequest(keyword: String?) -> URLRequest? {
         var urlString = "https://api.atsopt-seminar4.site/api/v1/users"
         
         if let keyword = keyword, !keyword.isEmpty,
@@ -30,12 +30,19 @@ final class GetInfoService {
         return request
     }
     
-    
-    func fetchNicknameList(keyword: String?) async throws -> [String] {
-        guard let request = makeRequest(keyword: keyword) else {
-            throw NetworkError.requestEncodingError
+    private func makeMyNicknameRequest(userId: Int) -> URLRequest? {
+        guard let url = URL(string: "https://api.atsopt-seminar4.site/api/v1/users/me") else {
+            return nil
         }
         
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("\(userId)", forHTTPHeaderField: "userId")
+        return request
+    }
+    
+    private func fetchNickname<T: Codable>(request: URLRequest, modelType: T.Type) async throws -> T {
         let (data, response) = try await URLSession.shared.data(for: request)
         
         guard let httpResponse = response as? HTTPURLResponse,
@@ -44,11 +51,27 @@ final class GetInfoService {
         }
         
         do {
-            let decoded = try JSONDecoder().decode(NicknameListResponseWrapper.self, from: data)
-            return decoded.data.nicknameList
+            let decoded = try JSONDecoder().decode(NicknameResponseWrapper<T>.self, from: data)
+            return decoded.data
         } catch {
             print("디코딩 실패: ", error)
             throw NetworkError.responseDecodingError
         }
+    }
+    
+    func fetchNicknameList(keyword: String?) async throws -> [String] {
+        guard let request = makeNicknameListRequest(keyword: keyword) else {
+            throw NetworkError.requestEncodingError
+        }
+        let result: NicknameListResponseModel = try await fetchNickname(request: request, modelType: NicknameListResponseModel.self)
+        return result.nicknameList
+    }
+    
+    func fetchMyNickname(userId: Int) async throws -> String {
+        guard let request = makeMyNicknameRequest(userId: userId) else {
+            throw NetworkError.requestEncodingError
+        }
+        let result: NicknameResponseModel = try await fetchNickname(request: request, modelType: NicknameResponseModel.self)
+        return result.nickname
     }
 }

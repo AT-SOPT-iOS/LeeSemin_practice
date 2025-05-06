979fe4b76c53acbfd296db33f90ad07737815003
sem-git <lsmsm0327@naver.com>
2025-05-06 15:41:18 +0900

    network: 닉네임 수정 데이터 모델 추가

---
+++ AT-SOPT-iOS-Practice/AT-SOPT-iOS-Practice/Week04/Network/Nickname/UpdateRequestModel.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateRequestModel.swift
+//  AT-SOPT-iOS-Practice
+//
+//  Created by 이세민 on 5/6/25.
+//
+
+import Foundation
+
+struct UpdateRequestModel: Codable {
+    let nickname: String
+}
